3cfad6161384ac6d5b4c2043cafe130a05e8ef88
Daniel Ancines <daniel.ancines@gmail.com>
2025-04-14 07:39:07 -0300

    DI Container Helper

---
+++ Maui.MemoryLeaksTests/DIContainerHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui.MemoryLeaksTests;
+
+public static class DIContainerHelper
+{
+    public static T? GetService<T>()
+    {
+#if WINDOWS
+        using var serviceScope = MauiWinUIApplication.Current.Services.CreateScope();
+        var serviceProvider = serviceScope.ServiceProvider;
+
+        return (T?)serviceProvider.GetService(typeof(T));
+#endif
+        return default;
+    }
+}
